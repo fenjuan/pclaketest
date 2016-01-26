af3c4ad03714a2d3d8e158ee0dbd9ff953b846af
fenjuan <fenjuan@bios.au.dk>
2016-01-26 16:00:43 +0100

    added make namelist script for gotm cases

--- gotm/make_namelists.sh
+++ gotm/make_namelists.sh
@@ -4,8 +4,7 @@ dirs="2m 5m 10m 20m"
 
 for d in $dirs; do
   cd $d
+  export GOTMDIR=~/GOTM/code
   make namelist
-  mv fabm_input.nml fabm_input.nml.keep
-  mv streams.nml streams.nml.keep
   cd ..
 done
