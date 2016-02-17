932ad523c28e7d929827a1bb92f64b1532524817
fenjuan <fenjuan@bios.au.dk>
2016-02-17 14:57:51 +0100

    setting up model to the last updaged gotm1d version

--- output/output-ana.py
+++ output/output-ana.py
@@ -14,7 +14,7 @@ import matplotlib.ticker as ticker
 import pandas as pd
 from pandas import DataFrame
 from pandas import Series
-# NetCDF library for reading model output and interface for exporting 
+# NetCDF library for reading model output and interface for exporting
 # NC file to normal arrays and pandas dataset
 from netCDF4 import Dataset
 from netCDF4 import num2date
@@ -35,19 +35,23 @@ import datetime
 files names for each type of model simulation is store as string array,
  fabm0d[], gotm1d[], gotmlake[], respectively
 """
-# Reading in model output files, from three different applications, fabm0d, 
+# Reading in model output files, from three different applications, fabm0d,
 # gotm1d and gotmlake
 output_dir= os.path.normpath(os.getcwd() + os.sep + os.pardir+"/output")
 # fabm0d files
-fabm0d=[]
+#fabm0d=['pclake-fabm0d-2m.nc','pclake-fabm0d-5m.nc','pclake-fabm0d-10m.nc' \
+#        'pclake-fabm0d-20m.nc']
+#gotm1d=['pclake-gotm1d-2m.nc','pclake-gotm1d-5m.nc','pclake-gotm1d-10m.nc', \
+#        'pclake-gotm1d-20m.nc']
+#gotmlake=['pclake-gotmlake-2m.nc','pclake-gotmlake-5m.nc',\
+#         'pclake-gotmlake-10m.nc','pclake-gotmlake-20m.nc']
+fabm0d=[];gotm1d=[];gotmlake=[]
 for f_0d in glob.glob("*fabm0d*.nc"):
     fabm0d.append(f_0d)
 # gotm1d files
-gotm1d=[]
 for f_1d in glob.glob("*gotm1d*.nc"):
     gotm1d.append(f_1d)
 # gotm-lake files
-gotmlake=[]
 for f_lake in glob.glob("*gotmlake*.nc"):
     gotmlake.append(f_lake)
 
@@ -61,9 +65,9 @@ Since 3 applications has the same time intervals, so use anyone of them is fine
 # Set ploting time intervals(keep matplotlib datetime format)
 path=os.path.join(output_dir, fabm0d[0])
 # convert NetCDF data to Dataset, to read time
-fabm0d_nc=Dataset(path, mode='r')
-time        = fabm0d_nc.variables['time']
-units       = fabm0d_nc.variables['time'].units
+time_nc=Dataset(path, mode='r')
+time        = time_nc.variables['time']
+units       = time_nc.variables['time'].units
 valid_times = num2date(time[:], units=units).tolist()
 # Set the start and stop point for ploting interval
 start=valid_times.index(datetime.datetime(2015, 1, 1))
@@ -81,7 +85,7 @@ time_t=mdates.drange(dt(2015,1,1), dt(2016,1,1), datetime.timedelta(days=1))
 """
 *****************Sectin for reading line plot data*****************************
 line plotting data are comparison with 3 different model set-ups: with 0D output
-(one value for whole water columne), 1D avaraged value(layer avaraged mean) and 
+(one value for whole water columne), 1D avaraged value(layer avaraged mean) and
 lake volumn avaraged value
 """
 # Read in gotm1d data, get the vertical avarage value, and store in nc Dataset form
@@ -111,7 +115,7 @@ for f_0d in fabm0d:
     sDZoo=DataFrame(Zoo,index=time,columns=['sDZoo'])
     aDFish=DataFrame(Fish,index=time,columns=['aDFish'])
     sDVeg=DataFrame(Veg,index=time,columns=['sDVeg'])
-    sDBent=DataFrame(Ben,index=time,columns=['sDBent'])   
+    sDBent=DataFrame(Ben,index=time,columns=['sDBent'])
 # put data into dict for future usage
     df_fabm0d[f_0d]=pd.concat([temp,PAR,O2,totP,totN,aDPhytW,sDZoo,aDFish,\
                               sDVeg,sDBent],axis=1)
@@ -192,7 +196,7 @@ for f_lake in gotmlake:
     Fish=[];Veg=[];Ben=[]
 # Loop over time, treat the data and put variables in lists
     i=0
-    for t in time:      
+    for t in time:
 # get the volume average data from gotmlake
         temperature.append(np.sum(gotmlake_nc.variables['temp'][i,:,0,0]*f_lvl[:]))
         Oxygen.append(np.sum(gotmlake_nc.variables['abiotic_water_sO2W'][i,:,0,0]*f_lvl[:]))
@@ -222,6 +226,7 @@ for f_lake in gotmlake:
 # Store gotmlake data into dict for future usage
     df_gotmlake[f_lake]=pd.concat([temp,PAR,O2,totP,totN,aDPhytW,sDZoo,aDFish,sDVeg,sDBent],axis=1)
 
+
 # join dataframe according to depths
 df_2m= pd.concat([df_fabm0d['pclake-fabm0d-2m.nc'],df_gotm1d['pclake-gotm1d-2m.nc'],
                   df_gotmlake['pclake-gotmlake-2m.nc']],axis=1,keys=['fabm0d', 'gotm1d', 'gotmlake'])
@@ -233,7 +238,6 @@ df_20m= pd.concat([df_fabm0d['pclake-fabm0d-20m.nc'],df_gotm1d['pclake-gotm1d-20
                    df_gotmlake['pclake-gotmlake-20m.nc']],axis=1,keys=['fabm0d', 'gotm1d', 'gotmlake'])
 # Joined all data together for plot
 results= pd.concat([df_2m,df_5m,df_10m,df_20m],axis=1,keys=['2m','5m','10m','20m'])
-
 """
 ***************Section for line plotting**************************************
 """
@@ -279,7 +283,7 @@ fontP.set_size('xx-small')
 # Plot line plots with three different model output
 # Plot group one variables
 fig1 = plt.figure(figsize=(8.27, 11.69), dpi=1200)
-fig1, axs = plt.subplots(4,4,sharex=True,squeeze=True, ) 
+fig1, axs = plt.subplots(4,4,sharex=True,squeeze=True, )
 # adjust the space between subplots
 fig1.subplots_adjust(hspace = 0.07)
 #plt.subplots_adjust(hspace = .1)
@@ -329,7 +333,7 @@ fig1.savefig('fig1.png',bbox_inches='tight')
 plt.close()
 # plot group two variables
 fig2 = plt.figure(figsize=(8.27, 11.69), dpi=1200)
-fig2, axs = plt.subplots(4,4,sharex=True) 
+fig2, axs = plt.subplots(4,4,sharex=True)
 j=0
 for depth in depths:
     ymin=ymin_2[j];ymax=ymax_2[j]
@@ -383,7 +387,7 @@ plt.close()
 
 df_1d={};df_lake={}
 for f_lake in gotmlake:
-    path_lake=os.path.join(output_dir, f_lake) 
+    path_lake=os.path.join(output_dir, f_lake)
     gotmlake_nc=Dataset(path_lake, mode='r')
 # Get the f_lvl for different depth
     lvl=len(gotmlake_nc.variables['temp'][0])
@@ -395,7 +399,8 @@ for f_lake in gotmlake:
     PAR_lake=gotmlake_nc.variables['phytoplankton_water_partop'][start:stop,:,0,0].T
     Phyto_lake=gotmlake_nc.variables['phytoplankton_water_aDPhytW'][start:stop,:,0,0].T
     Zoo_lake=gotmlake_nc.variables['foodweb_water_sDZoo'][start:stop,:,0,0].T
-    Fish_lake=gotmlake_nc.variables['foodweb_water_sDFiAd'][start:stop,:,0,0].T     +gotmlake_nc.variables['foodweb_water_sDFiJv'][start:stop,:,0,0].T
+    Fish_lake=gotmlake_nc.variables['foodweb_water_sDFiAd'][start:stop,:,0,0].T \
+              +gotmlake_nc.variables['foodweb_water_sDFiJv'][start:stop,:,0,0].T
     df_tm_lake=DataFrame(tm_lake,index=np.arange(lvl),columns=[time])
     df_O2_lake=DataFrame(O2_lake,index=np.arange(lvl), columns=[time])
     df_TN_lake=DataFrame(TN_lake,index=np.arange(lvl),columns=[time])
@@ -404,8 +409,11 @@ for f_lake in gotmlake:
     df_Phyto_lake=DataFrame(Phyto_lake,index=np.arange(lvl), columns=[time])
     df_Zoo_lake=DataFrame(Zoo_lake,index=np.arange(lvl), columns=[time])
     df_Fish_lake=DataFrame(Fish_lake,index=np.arange(lvl), columns=[time])
-    
-    df_lake[f_lake]=pd.concat([df_tm_lake,df_O2_lake,df_TN_lake,df_TP_lake,                                    df_PAR_lake,df_Phyto_lake,df_Zoo_lake,                                     df_Fish_lake],axis=1,keys=['tm','O2','TN','TP',                                                               'PAR','Phy','Zoo','Fis'])   
+
+    df_lake[f_lake]=pd.concat([df_tm_lake,df_O2_lake,df_TN_lake,df_TP_lake, \
+                                    df_PAR_lake,df_Phyto_lake,df_Zoo_lake, \
+                                     df_Fish_lake],axis=1,keys=['tm','O2','TN','TP',\
+                                                               'PAR','Phy','Zoo','Fis'])
 
 for f_1d in gotm1d:
     path_1d=os.path.join(output_dir, f_1d)
@@ -482,11 +490,11 @@ for var in group2:
         vmin_depths.append(np.min(vmin_models))
         vmax_depths.append(np.max(vmax_models))
     vmin_2.append(np.min(vmin_depths))
-    vmax_2.append(np.max(vmax_depths))    
+    vmax_2.append(np.max(vmax_depths))
 
 # starting plot group1 variables
 com_1d1 = plt.figure(figsize=(8.27, 11.69), dpi=1200)
-com_1d1, axs = plt.subplots(8,4,sharex=True,squeeze=True) 
+com_1d1, axs = plt.subplots(8,4,sharex=True,squeeze=True)
 # add color bar postions
 # add color bar, for group 1
 cbposition1=[]
@@ -521,7 +529,7 @@ for var in group1:
                 xtick.label.set_fontsize(5)
 # Set y-axis for subplots
             axs[i,j].yaxis.set_major_locator(ticker.FixedLocator([wd.min(),wd.max()]))
-            axs[i,j].set_yticklabels(['0',depth[k]])            
+            axs[i,j].set_yticklabels(['0',depth[k]])
             for ytick in axs[i,j].yaxis.get_major_ticks():
                 ytick.label.set_fontsize(5)
 # set labels for different model output
@@ -530,14 +538,14 @@ for var in group1:
                 axs[i,j].yaxis.set_label_coords(-0.25, 0.5)
 # plotting color bar
             if i==0:
-                cb=com_1d1.colorbar(cbmap,cax=cbposition1[j],orientation='horizontal') 
+                cb=com_1d1.colorbar(cbmap,cax=cbposition1[j],orientation='horizontal')
                 # set color bar location
                 cblocator=[];step=(vmax-vmin)/5;
                 cblocator=[vmin,vmin+step,vmin+2*step,vmin+3*step,vmin+4*step,vmin+5*step]
                 cb.set_ticks(cblocator)
                 cb.formatter=ticker.FormatStrFormatter(('%0.1f'))
                 cb.update_ticks()
-                cb.ax.tick_params(labelsize=5)                  
+                cb.ax.tick_params(labelsize=5)
             i=i+1
         k=k+1
     j=j+1
@@ -547,14 +555,13 @@ com_1d1.text(0.38,0.95,'Oxygen',**{'fontsize':8})
 com_1d1.text(0.55,0.95,'Total Nitrogen',**{'fontsize':8})
 com_1d1.text(0.75,0.95,'Total Phosphorus',**{'fontsize':8})
 com_1d1.autofmt_xdate(rotation=90)
-com_1d1.autofmt_xdate()
 #save fig
 com_1d1.savefig('comp_1d_1.png',bbox_inches='tight')
 plt.close()
 
 # Plotting variables group2
 com_1d2 = plt.figure(figsize=(8.27, 11.69), dpi=1200)
-com_1d2, axs = plt.subplots(8,4,sharex=True,squeeze=True) 
+com_1d2, axs = plt.subplots(8,4,sharex=True,squeeze=True)
 # add color bar postions
 # add color bar, for group 1
 cbposition2=[]
@@ -568,13 +575,11 @@ cbposition_4=com_1d2.add_axes([0.73, 0.93, 0.15,0.01])
 cbposition2.append(cbposition_4)
 # j for column location
 j=0
-for var in group1:
-    vmin=vmin_2[j]
-    vmax=vmax_2[j]
+for var in group2:
+    vmin=vmin_2[j];vmax=vmax_2[j]
 # i for row location
 # k for depths index
-    i=0
-    k=0
+    i=0;k=0
     for output in outputs:
         wd=np.arange(len(output.index))+1
         extent=[time_t.min(), time_t.max(),wd.min(),wd.max()]
@@ -597,30 +602,27 @@ for var in group1:
                 axs[i,j].yaxis.set_label_coords(-0.25, 0.5)
 # plotting color bar
             if i==0:
-                cb=com_1d2.colorbar(cbmap,cax=cbposition2[j],orientation='horizontal') 
+                cb=com_1d2.colorbar(cbmap,cax=cbposition2[j],orientation='horizontal')
                 # set color bar location
                 cblocator=[];step=(vmax-vmin)/5;
                 cblocator=[vmin,vmin+step,vmin+2*step,vmin+3*step,vmin+4*step,vmin+5*step]
                 cb.set_ticks(cblocator)
                 cb.formatter=ticker.FormatStrFormatter(('%0.1f'))
                 cb.update_ticks()
-                cb.ax.tick_params(labelsize=5)        
+                cb.ax.tick_params(labelsize=5)
             i=i+1
         k=k+1
     j=j+1
 # adjust a-axis labels
 # add columns for variables
-com_1d2.text(0.15,0.95,'PAR',**{'fontsize':8})
-com_1d2.text(0.38,0.95,'Phytoplankton',**{'fontsize':8})
+com_1d2.text(0.17,0.95,'PAR',**{'fontsize':8})
+com_1d2.text(0.36,0.95,'Phytoplankton',**{'fontsize':8})
 com_1d2.text(0.55,0.95,'Zooplankton',**{'fontsize':8})
-com_1d2.text(0.75,0.95,'Fish',**{'fontsize':8})
+com_1d2.text(0.77,0.95,'Fish',**{'fontsize':8})
 com_1d2.autofmt_xdate(rotation=90)
 
 com_1d2.savefig('comp_1d_2.png',bbox_inches='tight')
 
-plt.close()
-
 """
 **************************End of 2D plotting section***************************
 """
-
