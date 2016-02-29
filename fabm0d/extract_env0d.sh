ca439c2b23d76609d4cbab5d9398d35349e52284
fenjuan <fenjuan@bios.au.dk>
2016-02-29 14:53:53 +0100

    finishe developing script for plotting all plots in MS2, might need some futher development for formatting

--- fabm0d/extract_env0d.sh
+++ fabm0d/extract_env0d.sh
@@ -14,4 +14,10 @@ python gotm_to_0d_env.py env-5m.nc --lvl 12 >>  ../5m/env_5m.dat
 python gotm_to_0d_env.py env-10m.nc --lvl 24 >>  ../10m/env_10m.dat
 python gotm_to_0d_env.py env-20m.nc --lvl 49 >>  ../20m/env_20m.dat
 
+
+#python gotm_to_0d_env.py ../../output/pclake-gotm1d-2m.nc --lvl 4 >>  ../2m/env_2m.dat
+#python gotm_to_0d_env.py ../../output/pclake-gotm1d-5m.nc --lvl 12 >>  ../5m/env_5m.dat
+#python gotm_to_0d_env.py ../../output/pclake-gotm1d-10m.nc --lvl 24 >>  ../10m/env_10m.dat
+#python gotm_to_0d_env.py ../../output/pclake-gotm1d-20m.nc --lvl 49 >>  ../20m/env_20m.dat
+
 cd ..
